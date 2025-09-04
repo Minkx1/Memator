d76ea4d28e999c70f7a03675e0903ee663b688e9
Minkx1 <zakalata.mark@gmail.com>
2025-09-04 08:57:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def admin():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render передає порт у змінній PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0")
